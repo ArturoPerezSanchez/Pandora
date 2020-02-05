0f153eb39c3b60345b9195e7b5963bf17366c2cd
Arturo <rezekyt@mail.com>
2020-02-05 12:34:18 +0100

    feat(footer): responsive test 100% coverage

--- packages/pandora-footer/test/pandora-footer.test.js
+++ packages/pandora-footer/test/pandora-footer.test.js
@@ -77,11 +77,55 @@ describe('Footer', () => {
         ],
       },
     ];
-
     const el = await fixture(html`
       <pandora-footer .columns=${DATA}></pandora-footer>
     `);
 
     expect(el.columns).to.equal(DATA);
+  });
+
+  it('its responsive', async () => {
+    const DATA = [
+      {
+        title: 'Conócenos',
+        links: [
+          { title: 'Conoce la comunidad', href: '#' },
+          { title: 'Trabajar con nosotros', href: '#' },
+          { title: 'Sobre el colegio', href: '#' },
+        ],
+      },
+      {
+        title: 'Métodos de pago',
+        links: [
+          { title: 'Métodos de pago', href: '#' },
+          { title: 'Conversor de divisas', href: '#' },
+          { title: 'Cheques regalo', href: '#' },
+          { title: 'Recarga online', href: '#' },
+          { title: 'Recarga en tienda', href: '#' },
+        ],
+      },
+      {
+        title: '¿Necesitas ayuda?',
+        links: [
+          { title: 'Encuentra tu pedido', href: '#' },
+          { title: 'Tarifas y políticas de envio', href: '#' },
+          { title: 'Garantía de calidad', href: '#' },
+          { title: 'Devolver productos', href: '#' },
+          { title: 'Atención al cliente', href: '#' },
+        ],
+      },
+    ];
+    const el = await fixture(html`
+      <pandora-footer .columns=${DATA}></pandora-footer>
+    `);
+    el.shadowRoot.querySelectorAll('h4')[0].click();
+    expect(el.shadowRoot.querySelectorAll('h4')[0].classList[0]).to.equal('rotated');
+    el.shadowRoot.querySelectorAll('h4')[1].click();
+    expect(el.shadowRoot.querySelectorAll('h4')[0].classList[0]).to.equal(undefined);
+    expect(el.shadowRoot.querySelectorAll('h4')[1].classList[0]).to.equal('rotated');
+    el.shadowRoot.querySelectorAll('h4')[1].click();
+    expect(el.shadowRoot.querySelectorAll('h4')[1].classList[0]).to.equal(undefined);
+    el.shadowRoot.querySelectorAll('h4')[1].click();
+    expect(el.shadowRoot.querySelectorAll('h4')[1].classList[0]).to.equal('rotated');
   });
 });
