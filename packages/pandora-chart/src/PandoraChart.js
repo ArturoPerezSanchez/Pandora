7b4799f83e4d537237317acb5f4d7be31e7b0e43
Arturo <rezekyt@mail.com>
2020-03-02 07:21:13 +0100

    fix(chart): css, zoomable and array functionality

--- packages/pandora-chart/src/PandoraChart.js
+++ packages/pandora-chart/src/PandoraChart.js
@@ -1,38 +1,254 @@
 import { html, css, LitElement } from 'lit-element';
 import * as c3 from 'c3';
-import c3css from './c3.css';
 
 export class PandoraChart extends LitElement {
   static get styles() {
-    return [
-      c3css,
-      css`
-        :host {
-          display: block;
-        }
+    return css`
+      :host {
+        display: block;
+      }
 
-        .legend {
-          width: 100%;
-          font-size: 0.75em;
-          color: #555555;
-          font-family: 'Open Sans', sans-serif;
-        }
+      .legend {
+        width: 100%;
+        font-size: 0.75em;
+        color: #555555;
+        font-family: 'Open Sans', sans-serif;
+      }
 
-        .legend div {
-          cursor: pointer;
-          margin: 8px 0;
-        }
+      .legend div {
+        cursor: pointer;
+        margin: 8px 0;
+      }
 
-        .legend span {
-          float: left;
-          display: inline-block;
-          height: 10px;
-          width: 15px;
-          border-radius: 2px;
-          margin-right: 10px;
-        }
-      `,
-    ];
+      .legend span {
+        float: left;
+        display: inline-block;
+        height: 10px;
+        width: 15px;
+        border-radius: 2px;
+        margin-right: 10px;
+      }
+
+      .c3 svg {
+        font: 10px sans-serif;
+        -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+      }
+
+      .c3 path,
+      .c3 line {
+        fill: none;
+        stroke: #000;
+      }
+
+      .c3 text {
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      .c3-legend-item-tile,
+      .c3-xgrid-focus,
+      .c3-ygrid,
+      .c3-event-rect,
+      .c3-bars path {
+        shape-rendering: crispEdges;
+      }
+
+      .c3-chart-arc path {
+        stroke: #fff;
+      }
+
+      .c3-chart-arc rect {
+        stroke: white;
+        stroke-width: 1;
+      }
+
+      .c3-chart-arc text {
+        fill: #fff;
+        font-size: 13px;
+      }
+
+      .c3-grid line {
+        stroke: #aaa;
+      }
+
+      .c3-grid text {
+        fill: #aaa;
+      }
+
+      .c3-xgrid,
+      .c3-ygrid {
+        stroke-dasharray: 3 3;
+      }
+
+      .c3-text.c3-empty {
+        fill: #808080;
+        font-size: 2em;
+      }
+
+      .c3-line {
+        stroke-width: 1px;
+      }
+
+      .c3-circle._expanded_ {
+        stroke-width: 1px;
+        stroke: white;
+      }
+
+      .c3-selected-circle {
+        fill: white;
+        stroke-width: 2px;
+      }
+
+      .c3-bar {
+        stroke-width: 0;
+      }
+
+      .c3-bar._expanded_ {
+        fill-opacity: 1;
+        fill-opacity: 0.75;
+      }
+
+      .c3-target.c3-focused {
+        opacity: 1;
+      }
+
+      .c3-target.c3-focused path.c3-line,
+      .c3-target.c3-focused path.c3-step {
+        stroke-width: 2px;
+      }
+
+      .c3-target.c3-defocused {
+        opacity: 0.3 !important;
+      }
+
+      .c3-region {
+        fill: steelblue;
+        fill-opacity: 0.1;
+      }
+
+      .c3-brush .extent {
+        fill-opacity: 0.1;
+      }
+
+      .c3-legend-item {
+        font-size: 12px;
+      }
+
+      .c3-legend-item-hidden {
+        opacity: 0.15;
+      }
+
+      .c3-legend-background {
+        opacity: 0.75;
+        fill: white;
+        stroke: lightgray;
+        stroke-width: 1;
+      }
+
+      .c3-title {
+        font: 14px sans-serif;
+      }
+
+      .c3-tooltip-container {
+        z-index: 10;
+      }
+
+      .c3-tooltip {
+        border-collapse: collapse;
+        border-spacing: 0;
+        background-color: #fff;
+        empty-cells: show;
+        -webkit-box-shadow: 7px 7px 12px -9px #777777;
+        -moz-box-shadow: 7px 7px 12px -9px #777777;
+        box-shadow: 7px 7px 12px -9px #777777;
+        opacity: 0.9;
+      }
+
+      .c3-tooltip tr {
+        border: 1px solid #ccc;
+      }
+
+      .c3-tooltip th {
+        background-color: #aaa;
+        font-size: 14px;
+        padding: 2px 5px;
+        text-align: left;
+        color: #fff;
+      }
+
+      .c3-tooltip td {
+        font-size: 13px;
+        padding: 3px 6px;
+        background-color: #fff;
+        border-left: 1px dotted #999;
+      }
+
+      .c3-tooltip td > span {
+        display: inline-block;
+        width: 10px;
+        height: 10px;
+        margin-right: 6px;
+      }
+
+      .c3-tooltip .value {
+        text-align: right;
+      }
+
+      .c3-area {
+        stroke-width: 0;
+        opacity: 0.2;
+      }
+
+      .c3-chart-arcs-title {
+        dominant-baseline: middle;
+        font-size: 1.3em;
+      }
+
+      .c3-chart-arcs .c3-chart-arcs-background {
+        fill: #e0e0e0;
+        stroke: #fff;
+      }
+
+      .c3-chart-arcs .c3-chart-arcs-gauge-unit {
+        fill: #000;
+        font-size: 16px;
+      }
+
+      .c3-chart-arcs .c3-chart-arcs-gauge-max {
+        fill: #777;
+      }
+
+      .c3-chart-arcs .c3-chart-arcs-gauge-min {
+        fill: #777;
+      }
+
+      .c3-chart-arc .c3-gauge-value {
+        fill: #000;
+      }
+
+      .c3-chart-arc.c3-target g path {
+        opacity: 1;
+      }
+
+      .c3-chart-arc.c3-target.c3-focused g path {
+        opacity: 1;
+      }
+
+      .c3-drag-zoom.enabled {
+        pointer-events: all !important;
+        visibility: visible;
+      }
+
+      .c3-drag-zoom.disabled {
+        pointer-events: none !important;
+        visibility: hidden;
+      }
+
+      .c3-drag-zoom .extent {
+        fill-opacity: 0.1;
+      }
+    `;
   }
 
   static get properties() {
@@ -40,6 +256,8 @@ export class PandoraChart extends LitElement {
       type: { type: String },
       units: { type: String },
       endpointurl: { type: String },
+      data: { type: Array },
+      zoomable: { type: Boolean },
 
       _axis: { type: Array },
       _colors: { type: Array },
@@ -53,7 +271,9 @@ export class PandoraChart extends LitElement {
     this.type = 'bar';
     this.units = '';
     this.endpointurl = '';
+    this.zoomable = false;
 
+    this._data = [];
     this._error = '';
     this._response = [];
     this._axis = [];
@@ -72,36 +292,42 @@ export class PandoraChart extends LitElement {
 
   updated(changedProperties) {
     changedProperties.forEach((_, propName) => {
-      if (['_response', 'type', 'units'].includes(propName)) {
+      if (['_response', 'type', 'units', 'zoomable', '_axis', '_colors'].includes(propName)) {
         c3.generate(this.generateChart());
       }
-      if (['endpointurl'].includes(propName)) {
+      if (['endpointurl', 'data'].includes(propName)) {
         this.updatedResponse();
       }
     });
   }
 
   updatedResponse() {
-    fetch(this.endpointurl)
-      .then(response => {
-        if (response.ok) {
-          response
-            .json()
-            .then(json => {
-              this._response = json.data;
-              this._axis = json.axis;
-              this._colors = json.colors;
-            })
-            .catch(error => {
-              this._error = `ERROR_Json: ${error.message}`;
-            });
-        } else {
-          this._error = `Respuesta de red KO`;
-        }
-      })
-      .catch(error => {
-        this._error = `ERROR_Fetch: ${error.message}`;
-      });
+    if (this.data) {
+      this._response = this.data.data;
+      this._axis = this.data.axis;
+      this._colors = this.data.colors;
+    } else {
+      fetch(this.endpointurl)
+        .then(response => {
+          if (response.ok) {
+            response
+              .json()
+              .then(json => {
+                this._response = json.data;
+                this._axis = json.axis;
+                this._colors = json.colors;
+              })
+              .catch(error => {
+                this._error = `ERROR_Json: ${error.message}`;
+              });
+          } else {
+            this._error = `Respuesta de red KO`;
+          }
+        })
+        .catch(error => {
+          this._error = `ERROR_Fetch: ${error.message}`;
+        });
+    }
   }
 
   generateChart() {
@@ -119,7 +345,7 @@ export class PandoraChart extends LitElement {
           : ['#77B5C3', '#4863A3', '#9371AC', '#D87D7D', '#D9A46C', '#E3CD66'],
       },
       zoom: {
-        enabled: true,
+        enabled: this.zoomable,
       },
       axis: {
         x: {
--- packages/pandora-chart/stories/index.stories.js
+++ packages/pandora-chart/stories/index.stories.js
@@ -1,9 +1,9 @@
 import { storiesOf, html } from '@open-wc/demoing-storybook';
-import { withKnobs, text, select } from '@storybook/addon-knobs';
+import { withKnobs, text, select, object, boolean } from '@storybook/addon-knobs';
 import '../pandora-chart.js';
 import readme from '../README.md';
 
-const DATA_DOBLE = 'http://www.mocky.io/v2/5e5cf9f9320000770043c4ff';
+const ENDPOINTURL = 'http://www.mocky.io/v2/5e5cf9f9320000770043c4ff';
 const TYPE_OPTIONS = {
   line: 'line',
   spline: 'spline',
@@ -16,6 +16,17 @@ const TYPE_OPTIONS = {
   donut: 'donut',
   gauge: 'gauge',
 };
+const DATA = {
+  data: [
+    ['Educación', 25, 34, 12, 15],
+    ['Deuda pública', 21, 46, 12, 32],
+    ['Seguridad y protección social', 10, 9, 6, 14],
+    ['Agricultura y ganadería', 19, 24, 3, 32],
+    ['Sanidad', 25, 23, 33, 8],
+  ],
+  colors: ['#77B5C3', '#4863A3', '#9371AC', '#D87D7D', '#D9A46C', '#E3CD66'],
+  axis: ['Enero', 'Marzo', 'Junio', 'Septiembre'],
+};
 
 storiesOf('ÁTOMOS|pandora-chart', module)
   .addParameters({
@@ -25,81 +36,141 @@ storiesOf('ÁTOMOS|pandora-chart', module)
   .add('Line chart', () => {
     const TYPE = select('Type', TYPE_OPTIONS, 'line');
     const UNITS = text('units', '');
-    const DATA = text('data', DATA_DOBLE);
+    const ZOOMABLE = boolean('zoomable', true);
+    const DATA_ARRAY = object('data', DATA);
     return html`
-      <pandora-chart .type=${TYPE} .units=${UNITS} .endpointurl=${DATA}></pandora-chart>
+      <pandora-chart
+        .type=${TYPE}
+        .units=${UNITS}
+        .zoomable=${ZOOMABLE}
+        .data=${DATA_ARRAY}
+      ></pandora-chart>
     `;
   })
   .add('Spline Chart', () => {
     const TYPE = select('Type', TYPE_OPTIONS, 'spline');
     const UNITS = text('units', '');
-    const DATA = text('data', DATA_DOBLE);
+    const ZOOMABLE = boolean('zoomable', true);
+    const ENDPOINT = text('data', ENDPOINTURL);
     return html`
-      <pandora-chart .type=${TYPE} .units=${UNITS} .endpointurl=${DATA}></pandora-chart>
+      <pandora-chart
+        .type=${TYPE}
+        .units=${UNITS}
+        .zoomable=${ZOOMABLE}
+        .endpointurl=${ENDPOINT}
+      ></pandora-chart>
     `;
   })
   .add('Step Chart', () => {
     const TYPE = select('Type', TYPE_OPTIONS, 'step');
     const UNITS = text('units', '');
-    const DATA = text('data', DATA_DOBLE);
+    const ZOOMABLE = boolean('zoomable', false);
+    const ENDPOINT = text('data', ENDPOINTURL);
     return html`
-      <pandora-chart .type=${TYPE} .units=${UNITS} .endpointurl=${DATA}></pandora-chart>
+      <pandora-chart
+        .type=${TYPE}
+        .units=${UNITS}
+        .zoomable=${ZOOMABLE}
+        .endpointurl=${ENDPOINT}
+      ></pandora-chart>
     `;
   })
   .add('Area Chart', () => {
     const TYPE = select('Type', TYPE_OPTIONS, 'area');
     const UNITS = text('units', '');
-    const DATA = text('data', DATA_DOBLE);
+    const ZOOMABLE = boolean('zoomable', true);
+    const ENDPOINT = text('data', ENDPOINTURL);
     return html`
-      <pandora-chart .type=${TYPE} .units=${UNITS} .endpointurl=${DATA}></pandora-chart>
+      <pandora-chart
+        .type=${TYPE}
+        .units=${UNITS}
+        .zoomable=${ZOOMABLE}
+        .endpointurl=${ENDPOINT}
+      ></pandora-chart>
     `;
   })
   .add('Area-spline Chart', () => {
     const TYPE = select('Type', TYPE_OPTIONS, 'area-spline');
     const UNITS = text('units', '');
-    const DATA = text('data', DATA_DOBLE);
+    const ZOOMABLE = boolean('zoomable', true);
+    const ENDPOINT = text('data', ENDPOINTURL);
     return html`
-      <pandora-chart .type=${TYPE} .units=${UNITS} .endpointurl=${DATA}></pandora-chart>
+      <pandora-chart
+        .type=${TYPE}
+        .units=${UNITS}
+        .zoomable=${ZOOMABLE}
+        .endpointurl=${ENDPOINT}
+      ></pandora-chart>
     `;
   })
   .add('Bar Chart', () => {
     const TYPE = select('Type', TYPE_OPTIONS, 'bar');
     const UNITS = text('units', '');
-    const DATA = text('data', DATA_DOBLE);
+    const ZOOMABLE = boolean('zoomable', true);
+    const ENDPOINT = text('data', ENDPOINTURL);
     return html`
-      <pandora-chart .type=${TYPE} .units=${UNITS} .endpointurl=${DATA}></pandora-chart>
+      <pandora-chart
+        .type=${TYPE}
+        .units=${UNITS}
+        .zoomable=${ZOOMABLE}
+        .endpointurl=${ENDPOINT}
+      ></pandora-chart>
     `;
   })
 
   .add('Scatter Chart', () => {
     const TYPE = select('Type', TYPE_OPTIONS, 'scatter');
     const UNITS = text('units', '');
-    const DATA = text('data', DATA_DOBLE);
+    const ZOOMABLE = boolean('zoomable', false);
+    const ENDPOINT = text('data', ENDPOINTURL);
     return html`
-      <pandora-chart .type=${TYPE} .units=${UNITS} .endpointurl=${DATA}></pandora-chart>
+      <pandora-chart
+        .type=${TYPE}
+        .units=${UNITS}
+        .zoomable=${ZOOMABLE}
+        .endpointurl=${ENDPOINT}
+      ></pandora-chart>
     `;
   })
   .add('Pie Chart', () => {
     const TYPE = select('Type', TYPE_OPTIONS, 'pie');
     const UNITS = text('units', '');
-    const DATA = text('data', DATA_DOBLE);
+    const ZOOMABLE = boolean('zoomable', false);
+    const ENDPOINT = text('data', ENDPOINTURL);
     return html`
-      <pandora-chart .type=${TYPE} .units=${UNITS} .endpointurl=${DATA}></pandora-chart>
+      <pandora-chart
+        .type=${TYPE}
+        .units=${UNITS}
+        .zoomable=${ZOOMABLE}
+        .endpointurl=${ENDPOINT}
+      ></pandora-chart>
     `;
   })
   .add('Donut Chart', () => {
     const TYPE = select('Type', TYPE_OPTIONS, 'donut');
     const UNITS = text('units', '');
-    const DATA = text('data', DATA_DOBLE);
+    const ZOOMABLE = boolean('zoomable', false);
+    const ENDPOINT = text('data', ENDPOINTURL);
     return html`
-      <pandora-chart .type=${TYPE} .units=${UNITS} .endpointurl=${DATA}></pandora-chart>
+      <pandora-chart
+        .type=${TYPE}
+        .units=${UNITS}
+        .zoomable=${ZOOMABLE}
+        .endpointurl=${ENDPOINT}
+      ></pandora-chart>
     `;
   })
   .add('Gauge Chart', () => {
     const TYPE = select('Type', TYPE_OPTIONS, 'gauge');
     const UNITS = text('units', '');
-    const DATA = text('data', DATA_DOBLE);
+    const ZOOMABLE = boolean('zoomable', false);
+    const ENDPOINT = text('data', ENDPOINTURL);
     return html`
-      <pandora-chart .type=${TYPE} .units=${UNITS} .endpointurl=${DATA}></pandora-chart>
+      <pandora-chart
+        .type=${TYPE}
+        .units=${UNITS}
+        .zoomable=${ZOOMABLE}
+        .endpointurl=${ENDPOINT}
+      ></pandora-chart>
     `;
   });
