a942903bde0eaa18b9043ba26776000564874dc3
ArturoPerezSanchez <rezekyt@gmail.com>
2020-08-20 11:51:49 +0200

    feat(icons): first stable version

--- packages/pandora-icons/src/PandoraIcons.js
+++ packages/pandora-icons/src/PandoraIcons.js
@@ -1,36 +1,88 @@
 import { html, css, LitElement } from 'lit-element';
+import { library, dom, icon } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fac } from './custom-icons.js';
+
+// Add all icons to the library so you can use it in your page
+library.add(fas, far, fab, fac);
 
 export class PandoraIcons extends LitElement {
   static get styles() {
     return css`
-      .container {
-        width: 100%;
+      :host {
+        display: inline-block;
       }
     `;
   }
 
   static get properties() {
     return {
+      // Icons
+      family: { type: String },
       icon: { type: String },
+      color: { type: String },
+
+      // Classes
+      spin: { type: Boolean },
+      pulse: { type: Boolean },
+      flip: { type: String },
+      size: { type: String },
+      rotation: { type: String },
     };
   }
 
   constructor() {
     super();
+    this.family = 'fas';
     this.icon = '';
+
+    this.color = '#333';
+    this.spin = false;
+    this.pulse = false;
+    this.flip = '';
+    this.size = 'lg';
+    this.rotation = '';
+  }
+
+  get classes() {
+    const { spin, pulse, flip, size, rotation } = this;
+
+    const classes = {
+      'fa-spin': spin,
+      'fa-pulse': pulse,
+      'fa-flip-horizontal': flip === 'horizontal' || flip === 'both',
+      'fa-flip-vertical': flip === 'vertical' || flip === 'both',
+      [`fa-${size}`]: size !== null,
+      [`fa-rotate-${rotation}`]: rotation !== null,
+    };
+
+    return Object.keys(classes)
+      .map(key => (classes[key] ? key : null))
+      .filter(key => key);
+  }
+
+  fontawesome() {
+    const { family: prefix, icon: iconName, classes } = this;
+
+    const faIcon = icon(
+      { prefix, iconName },
+      {
+        classes: [...classes],
+      },
+    );
+    return html(faIcon.html);
   }
 
   render() {
     return html`
-      <div class="container"></div>
+      <style>
+        ${dom.css()}
+      </style>
+      <div style="color:${this.color}">
+        ${this.fontawesome()}
+      </div>
     `;
-  }
-
-  updated(changedProperties) {
-    changedProperties.forEach((_, propName) => {
-      if ([''].includes(propName)) {
-        this.updatedResponse();
-      }
-    });
   }
 }
--- packages/pandora-icons/stories/index.stories.js
+++ packages/pandora-icons/stories/index.stories.js
@@ -1,16 +1,108 @@
 import { storiesOf, html } from '@open-wc/demoing-storybook';
-import { withKnobs } from '@storybook/addon-knobs';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { withKnobs, text, boolean, select, color } from '@storybook/addon-knobs';
 import '../pandora-icons.js';
 import readme from '../README.md';
 
-storiesOf('DISPLAY|pandora-icons', module)
+const iconOptions = {
+  angular: 'angular',
+  'electronic arts': 'ea',
+  git: 'git',
+  heart: 'heart',
+  nike: 'nike',
+};
+
+const flipOtions = {
+  '': '',
+  both: 'both',
+  horizontal: 'horizontal',
+  vertical: 'vertical',
+};
+
+const rotationOptions = {
+  '': '',
+  '90': '90',
+  '180': '180',
+  '270': '270',
+};
+
+const sizeOptions = {
+  xs: 'xs',
+  sm: 'sm',
+  lg: 'lg',
+  '2x': '2x',
+  '3x': '3x',
+  '4x': '4x',
+  '5x': '5x',
+  '6x': '6x',
+  '7x': '7x',
+  '8x': '8x',
+  '9x': '9x',
+  '10x': '10x',
+};
+
+storiesOf('BRAND|pandora-icons', module)
   .addParameters({
     notes: { markdown: readme },
   })
   .addDecorator(withKnobs)
-  .add(
-    'Default icon',
-    () => html`
-      <pandora-icons></pandora-icons>
-    `,
-  );
+  .add('Default icon', () => {
+    const FAMILY = text('family', 'fab');
+    const ICON = text('icon', 'bluetooth');
+
+    return html`
+      <div style="margin: 30px;">
+        <pandora-icons .family=${FAMILY} .icon=${ICON}></pandora-icons>
+      </div>
+    `;
+  })
+  .add('Custom icon font awesome', () => {
+    const FAMILY = text('family', 'fas');
+    const ICON = text('icon', 'american-sign-language-interpreting');
+    const COLOR = color('color', '#f28');
+    const SPIN = boolean('spin', false);
+    const PULSE = boolean('pulse', false);
+    const SIZE = select('size', sizeOptions, '5x');
+    const ROTATION = select('rotation', rotationOptions, '');
+    const FLIP = select('flip', flipOtions, '');
+
+    return html`
+      <div style="margin: 30px;">
+        <pandora-icons
+          .family=${FAMILY}
+          .icon=${ICON}
+          .color=${COLOR}
+          .spin=${SPIN}
+          .pulse=${PULSE}
+          .flip=${FLIP}
+          .size=${SIZE}
+          .rotation=${ROTATION}
+        ></pandora-icons>
+      </div>
+    `;
+  })
+  .add('Custom icon from scratch', () => {
+    const FAMILY = text('family', 'fac');
+    const ICON = select('icon', iconOptions, 'git');
+    const COLOR = color('color', 'black');
+    const SPIN = boolean('spin', false);
+    const PULSE = boolean('pulse', false);
+    const SIZE = select('size', sizeOptions, '5x');
+    const ROTATION = select('rotation', rotationOptions, '');
+    const FLIP = select('flip', flipOtions, '');
+
+    return html`
+      <div style="margin: 30px;">
+        <pandora-icons
+          .family=${FAMILY}
+          .icon=${ICON}
+          .color=${COLOR}
+          .spin=${SPIN}
+          .pulse=${PULSE}
+          .flip=${FLIP}
+          .size=${SIZE}
+          .rotation=${ROTATION}
+        ></pandora-icons>
+      </div>
+    `;
+  });
