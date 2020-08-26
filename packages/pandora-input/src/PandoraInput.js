2fc44a4e13882da59eecfaa463aa99f5e745a665
ArturoPerezSanchez <rezekyt@gmail.com>
2020-08-26 14:56:57 +0200

    feat(input): first stable version

--- packages/pandora-input/src/PandoraInput.js
+++ packages/pandora-input/src/PandoraInput.js
@@ -6,39 +6,89 @@ export class PandoraInput extends LitElement {
       .container {
         width: 100%;
       }
+      input {
+        width: 100%;
+        border: solid;
+      }
+      input:focus {
+        outline: none;
+      }
     `;
   }
 
   static get properties() {
     return {
+      type: { type: String },
       placeholder: { type: String },
       textcolor: { type: String },
       backgroundcolor: { type: String },
-      activebackgroundcolor: { type: String },
-      activetextcolor: { type: String },
-      width: { type: String },
+      textalign: { type: String },
+      padding: { typer: String },
       textsize: { type: String },
-      borderradious: { type: String },
-      bordercolor: { type: String },
+      fontweight: { type: String },
+      borderradius: { type: String },
+      border: { type: String },
+      value: { type: String },
+      labelText: { type: String },
+      name: { type: String },
+
+      pattern: { type: String },
+      maxlength: { type: Number },
+      required: { type: Boolean },
+      readonly: { type: Boolean },
+      disabled: { type: Boolean },
     };
   }
 
   constructor() {
     super();
     this.placeholder = '';
-    this.textcolor = 'green';
-    this.backgroundcolor = 'white';
-    this.activebackgroundcolor = 'white';
-    this.activetextcolor = 'black';
-    this.width = '100px';
-    this.textsize = '12px';
-    this.borderradious = '0%';
-    this.bordercolor = 'black';
+    this.textcolor = 'black';
+    this.backgroundcolor = '#ccc';
+    this.textsize = '18px';
+    this.fontweight = '200';
+    this.borderradius = '9px';
+    this.border = '3px solid #333';
+    this.textalign = 'center';
+    this.labelText = '';
+    this.name = 'pandoraInput';
+    this.padding = '5px';
+    this.value = '';
+    this.pattern = '';
+    this.maxlength = 25;
+    this.disabled = false;
+    this.required = true;
+    this.readonly = false;
+    this.type = 'text'; // url, text, search, email, password, tel
   }
 
   render() {
     return html`
-      <div class="container"></div>
+      <div class="container">
+        <label for=${this.name}>${this.labelText}</label>
+        <input
+          aria-label=${this.name}
+          name=${this.name}
+          type=${this.type}
+          value=${this.value}
+          pattern=${this.pattern}
+          maxlength=${this.maxlength}
+          ?disabled=${this.disabled}
+          ?required=${this.required}
+          ?readonly=${this.readonly}
+          placeholder=${this.placeholder}
+          style="
+            color:${this.textcolor};
+            background:${this.backgroundcolor};
+            text-align:${this.textalign};
+            font-size:${this.textsize};
+            border-radius:${this.borderradius};
+            border:${this.border};
+            padding:${this.padding};
+            font-weight:${this.fontweight};
+          "
+        />
+      </div>
     `;
   }
 
