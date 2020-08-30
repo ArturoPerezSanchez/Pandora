c225374059a821e6ce7e018fb0b8c0f1e271daaf
ArturoPerezSanchez <rezekyt@gmail.com>
2020-08-30 19:43:55 +0200

    feat(radio-button): first stable version

--- packages/pandora-radio-button/src/PandoraRadioButton.js
+++ packages/pandora-radio-button/src/PandoraRadioButton.js
@@ -6,6 +6,11 @@ export class PandoraRadioButton extends LitElement {
       .container {
         width: 100%;
       }
+      legend {
+        float: left;
+        width: 100%;
+        text-align: center;
+      }
     `;
   }
 
@@ -13,52 +18,89 @@ export class PandoraRadioButton extends LitElement {
     return {
       options: { type: Array },
       endpointURL: { type: String },
-
       text: { type: String },
+
       textcolor: { type: String },
       backgroundcolor: { type: String },
-      activebackgroundcolor: { type: String },
       textsize: { type: String },
-      borderradious: { type: String },
-      bordercolor: { type: String },
+      fontweight: { type: String },
+      borderradius: { type: String },
+      border: { type: String },
+      textalign: { type: String },
+      labelText: { type: String },
+      name: { type: String },
+      disabled: { type: Boolean },
+      required: { type: Boolean },
+      readonly: { type: Boolean },
 
       _data: { type: Array },
-      _error: { type: String },
     };
   }
 
   constructor() {
     super();
-    this.options = [];
     this.endpointURL = '';
+    this.text = 'select an option';
 
-    this.text = '';
-    this.textcolor = 'green';
-    this.backgroundcolor = 'white';
-    this.activebackgroundcolor = 'white';
-    this.textsize = '12px';
-    this.borderradious = '0%';
-    this.bordercolor = 'black';
+    this.textcolor = 'black';
+    this.backgroundcolor = '#ccc';
+    this.textsize = '18px';
+    this.fontweight = '200';
+    this.borderradius = '3px';
+    this.border = '3px solid #333';
+    this.textalign = 'left';
+    this.labelText = 'Checkbox Label';
+    this.name = 'pandoraInput';
+    this.disabled = false;
+    this.required = true;
+    this.readonly = false;
 
     this._data = [];
-    this._error = '';
+    this._error = null;
   }
 
   render() {
     return html`
-      <div class="container"></div>
+      ${this._error
+        ? html`
+            ${this.error}
+          `
+        : html`
+            <fieldset
+              class="container"
+              style="
+          color:${this.textcolor};
+          background:${this.backgroundcolor};
+          text-align:${this.textalign};
+          font-size:${this.textsize};
+          border-radius:${this.borderradius};
+          border:${this.border};
+          font-weight:${this.fontweight};"
+            >
+              <legend>${this.text}</legend>
+              ${this._data.map(
+                (dato, index) => html`
+                  <br /><label aria-label=${this.name} for=${this.name + index} name=${this.name}
+                    >${dato}</label
+                  >
+                  <input type="radio" aria-label=${this.name} name=${this.name} />
+                `,
+              )}
+            </fieldset>
+          `}
     `;
   }
 
   updated(changedProperties) {
     changedProperties.forEach((_, propName) => {
-      if ([''].includes(propName)) {
+      if (['options', 'endpointURL'].includes(propName)) {
         this.updatedResponse();
       }
     });
   }
 
   updatedResponse() {
+    this._error = null;
     if (this.options) {
       this._data = this.options;
     } else {
